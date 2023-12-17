586b0b99c92c2cff0c0dcdb1878dfbf3b0c31ce3
Mostafa Mahmoud Salah <mm4581@fayoum.edu.eg>
2023-12-17 14:11:44 +0200

    first commit

---
+++ src/screens/HomePage/HomePage.jsx
@@ -0,0 +1,356 @@
+import React from "react";
+import "./style.css";
+
+export const HomePage = () => {
+  return (
+    <div className="home-page">
+      <div className="div">
+        <div className="overlap">
+          <div className="overlap-group">
+            <div className="overlap-2">
+              <img className="group" alt="Group" src="/img/group.png" />
+              <img className="img" alt="Group" src="/img/group-17.png" />
+              <img className="mask-group" alt="Mask group" src="/img/mask-group-3.png" />
+              <img className="dish" alt="Dish" src="/img/dish-2-1.png" />
+            </div>
+            <div className="navbar">
+              <div className="text-wrapper">Menu</div>
+              <div className="text-wrapper-2">Gallery</div>
+              <div className="text-wrapper-3">About</div>
+              <div className="text-wrapper-4">Contact</div>
+              <div className="text-wrapper-5">Events</div>
+            </div>
+            <div className="overlap-3">
+              <p className="we-provide-the-best">
+                We provide the <br />
+                best food for you
+              </p>
+              <div className="ellipse" />
+              <div className="ellipse-2" />
+              <div className="ellipse-3" />
+              <img className="logo" alt="Logo" src="/img/logo-1-1.svg" />
+            </div>
+            <img className="group-2" alt="Group" src="/img/group-16.png" />
+          </div>
+          <div className="navigation-button">
+            <div className="div-wrapper">
+              <div className="text-wrapper-6">Book a table</div>
+            </div>
+          </div>
+          <div className="overlap-wrapper">
+            <div className="overlap-4">
+              <div className="text-wrapper-7">Book a table</div>
+            </div>
+          </div>
+          <div className="overlap-group-wrapper">
+            <div className="overlap-5">
+              <div className="text-wrapper-8">Menu</div>
+            </div>
+          </div>
+          <div className="facebook">
+            <img className="image" alt="Image" src="/img/image-1-1.png" />
+          </div>
+          <div className="image-wrapper">
+            <img className="image" alt="Image" src="/img/image-2.png" />
+          </div>
+          <div className="instagram-wrapper">
+            <img className="instagram" alt="Instagram" src="/img/icons8-instagram-1.svg" />
+          </div>
+          <img className="line" alt="Line" src="/img/line-1.svg" />
+          <p className="p">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+            dolore magna aliqua.
+          </p>
+        </div>
+        <div className="overlap-6">
+          <div className="overlap-7">
+            <div className="navigation-button-2">
+              <div className="overlap-8">
+                <div className="text-wrapper-7">Book a table</div>
+              </div>
+              <div className="stars">
+                <div className="text-wrapper-9">star</div>
+                <div className="text-wrapper-10">star</div>
+                <div className="text-wrapper-11">star</div>
+                <div className="text-wrapper-12">star</div>
+                <div className="text-wrapper-13">star</div>
+              </div>
+            </div>
+            <div className="navigation-button-3">
+              <div className="overlap-9">
+                <div className="text-wrapper-8">Menu</div>
+              </div>
+            </div>
+            <div className="rectangle" />
+            <img className="dish-2" alt="Dish" src="/img/dish-2-2.png" />
+            <div className="text-wrapper-14">Our Special Dishes</div>
+            <div className="text-wrapper-15">Wecome to Our Restaurant</div>
+            <p className="text-wrapper-16">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+            </p>
+            <p className="text-wrapper-17">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+              dolore magna aliqua.
+            </p>
+            <div className="food-card">
+              <div className="overlap-10">
+                <div className="overlap-11">
+                  <img className="group-3" alt="Group" src="/img/group-15.png" />
+                  <img className="rectangle-2" alt="Rectangle" src="/img/rectangle-7-3.svg" />
+                  <img className="dish-3" alt="Dish" src="/img/dish-1-1.png" />
+                  <p className="text-wrapper-18">
+                    Lorem ipsum dolor sit , consectetur adipiscing elit, sed do eiusmod tempor
+                  </p>
+                  <div className="text-wrapper-19">Lumpia with Suace</div>
+                </div>
+                <div className="price">
+                  <div className="overlap-group-2">
+                    <div className="text-wrapper-20">$12</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="food-card-2">
+              <div className="overlap-12">
+                <div className="overlap-13">
+                  <img className="rectangle-2" alt="Rectangle" src="/img/rectangle-7.svg" />
+                  <p className="text-wrapper-18">
+                    Lorem ipsum dolor sit , consectetur adipiscing elit, sed do eiusmod tempor
+                  </p>
+                  <div className="text-wrapper-21">Fish and Veggie</div>
+                  <img className="dish-4" alt="Dish" src="/img/dish-3-1.png" />
+                </div>
+                <div className="price">
+                  <div className="overlap-group-2">
+                    <div className="text-wrapper-20">$12</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="food-card-3">
+              <div className="overlap-12">
+                <div className="overlap-13">
+                  <img className="rectangle-2" alt="Rectangle" src="/img/rectangle-7.svg" />
+                  <p className="text-wrapper-18">
+                    Lorem ipsum dolor sit , consectetur adipiscing elit, sed do eiusmod tempor
+                  </p>
+                  <img className="dish-3" alt="Dish" src="/img/dish-5.png" />
+                  <div className="text-wrapper-22">Tofu Chili</div>
+                </div>
+                <div className="price">
+                  <div className="overlap-group-2">
+                    <div className="text-wrapper-20">$12</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="food-card-4">
+              <div className="overlap-12">
+                <div className="overlap-13">
+                  <img className="rectangle-2" alt="Rectangle" src="/img/rectangle-7.svg" />
+                  <p className="text-wrapper-18">
+                    Lorem ipsum dolor sit , consectetur adipiscing elit, sed do eiusmod tempor
+                  </p>
+                  <div className="text-wrapper-23">Egg and Cocumber</div>
+                  <img className="dish-3" alt="Dish" src="/img/dish-4.png" />
+                </div>
+                <div className="price">
+                  <div className="overlap-group-2">
+                    <div className="text-wrapper-20">$12</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <img className="leaf" alt="Leaf" src="/img/leaf-3.png" />
+            <img className="group-4" alt="Group" src="/img/group-13.png" />
+            <img className="group-5" alt="Group" src="/img/group-13.png" />
+            <img className="group-6" alt="Group" src="/img/group-11.png" />
+            <img className="group-7" alt="Group" src="/img/group-8.png" />
+            <img className="group-8" alt="Group" src="/img/group-8.png" />
+            <img className="group-9" alt="Group" src="/img/group-7.png" />
+            <img className="group-10" alt="Group" src="/img/group-6.png" />
+            <img className="group-11" alt="Group" src="/img/group-5.png" />
+            <img className="group-12" alt="Group" src="/img/group-4.png" />
+            <img className="chef" alt="Chef" src="/img/chef-1.png" />
+            <div className="rectangle-3" />
+            <div className="text-wrapper-24">Our Happy Customers</div>
+            <p className="text-wrapper-25">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+            </p>
+            <img className="group-13" alt="Group" src="/img/group-3.png" />
+            <div className="rectangle-4" />
+            <div className="testimonial">
+              <div className="overlap-14">
+                <img className="rectangle-5" alt="Rectangle" src="/img/rectangle-10.svg" />
+                <img className="mask-group-2" alt="Mask group" src="/img/mask-group-2.png" />
+                <div className="testimonial-text">
+                  <p className="text-wrapper-26">
+                    Lorem ipsum dolor sit amet consectetur. Tortor massa nisl quam sit. Vitae congue ultrices neque
+                    penatibus mi in quisque. Leo in cursus enim magnis ante. Proin iaculis platea ipsum sagittis ac eu
+                    aliquam quis. Ornare tincidunt tempus semper
+                  </p>
+                  <div className="overlap-group-3">
+                    <div className="text-wrapper-27">Ama Ampomah</div>
+                    <div className="CEO-founder-inc">CEO &amp; Founder Inc</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="testimonial-2">
+              <div className="overlap-14">
+                <img className="rectangle-5" alt="Rectangle" src="/img/rectangle-10.svg" />
+                <img className="mask-group-3" alt="Mask group" src="/img/mask-group-1.png" />
+                <div className="testimonial-text-2">
+                  <p className="text-wrapper-26">
+                    Lorem ipsum dolor sit amet consectetur. Tortor massa nisl quam sit. Vitae congue ultrices neque
+                    penatibus mi in quisque. Leo in cursus enim magnis ante. Proin iaculis platea ipsum sagittis ac eu
+                    aliquam quis. Ornare tincidunt tempus semper
+                  </p>
+                  <div className="overlap-group-4">
+                    <div className="text-wrapper-28">Kweku Annan</div>
+                    <div className="CEO-founder-inc">CEO &amp; Founder Inc</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="stars-2">
+              <div className="text-wrapper-9">star</div>
+              <div className="text-wrapper-29">star</div>
+              <div className="text-wrapper-30">star</div>
+              <div className="text-wrapper-31">star</div>
+              <div className="text-wrapper-32">star</div>
+            </div>
+            <div className="stars-3">
+              <div className="text-wrapper-33">star</div>
+              <div className="text-wrapper-34">star</div>
+              <div className="text-wrapper-35">star</div>
+              <div className="text-wrapper-36">star</div>
+            </div>
+            <div className="dot">
+              <div className="ellipse-4" />
+              <div className="ellipse-5" />
+              <div className="ellipse-6" />
+              <div className="ellipse-7" />
+              <div className="ellipse-8" />
+              <div className="ellipse-9" />
+            </div>
+            <img className="mask-group-4" alt="Mask group" src="/img/mask-group.png" />
+            <p className="get-or-promo-code-by">
+              Get Or Promo Code by
+              <br />
+              Subscribing To our&nbsp;&nbsp;Newsletter
+            </p>
+            <div className="rectangle-6" />
+            <div className="group-14">
+              <div className="overlap-15">
+                <div className="rectangle-7" />
+                <div className="text-wrapper-37">Subscribe</div>
+              </div>
+              <div className="text-wrapper-38">Enter your email</div>
+            </div>
+            <img className="leaf-2" alt="Leaf" src="/img/leaf-1.png" />
+            <img className="group-15" alt="Group" src="/img/group-1.png" />
+          </div>
+          <div className="navigation-button-4">
+            <div className="overlap-16">
+              <div className="text-wrapper-39">Book a table</div>
+            </div>
+          </div>
+          <div className="navigation-button-5">
+            <div className="overlap-17">
+              <div className="text-wrapper-40">Menu</div>
+            </div>
+          </div>
+          <div className="overlap-18">
+            <div className="text-wrapper-41">Our Expects Chef</div>
+            <p className="text-wrapper-42">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+              dolore magna aliqua.
+            </p>
+            <img className="group-16" alt="Group" src="/img/group-10.png" />
+          </div>
+          <img className="group-17" alt="Group" src="/img/group-12.png" />
+        </div>
+        <div className="overlap-19">
+          <img className="group-18" alt="Group" src="/img/group-2.png" />
+          <div className="copyrighter-section">
+            <div className="overlap-20">
+              <img className="icons-instagram" alt="Instagram" src="/img/icons8-instagram-2.svg" />
+              <img className="image-2" alt="Image" src="/img/image-3.png" />
+              <div className="group-19">
+                <div className="img-wrapper">
+                  <img className="image-3" alt="Image" src="/img/image-1.png" />
+                </div>
+                <div className="facebook-2" />
+                <div className="facebook-3" />
+                <div className="text-wrapper-43">FOLLOW US</div>
+              </div>
+            </div>
+            <p className="lorem-ipsum-dolor">
+              <span className="span">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                dolore .
+              </span>
+              <span className="text-wrapper-44">Learn more</span>
+            </p>
+            <p className="monday-friday">
+              Monday - Friday
+              <br />
+              8:00 am to 9:00 pm
+            </p>
+            <p className="saturday-am-to">
+              Saturday
+              <br />
+              8:00 am to 9:00 pm
+            </p>
+            <div className="sunday-CLOSED">
+              Sunday
+              <br />
+              CLOSED
+            </div>
+            <div className="group-20">
+              <div className="overlap-group-5">
+                <div className="menu-about-us">
+                  Menu
+                  <br />
+                  About us
+                  <br />
+                  Contact us
+                  <br />
+                  Main dishes
+                </div>
+                <div className="text-wrapper-43">NAVIGATION</div>
+              </div>
+            </div>
+            <div className="text-wrapper-45">OPENING HOURS</div>
+            <div className="group-21">
+              <div className="overlap-21">
+                <p className="fish-viggies-tofu">
+                  Fish &amp; Viggies
+                  <br />
+                  Tofu Chili
+                  <br />
+                  Egg &amp; Cocumber
+                  <br />
+                  Lumpia w/Suace
+                </p>
+                <div className="text-wrapper-43">DISHES</div>
+              </div>
+            </div>
+            <img className="logo-2" alt="Logo" src="/img/logo-1.svg" />
+            <img className="line-2" alt="Line" src="/img/line-2.svg" />
+            <div className="copyright">
+              <img className="copyright-symbol" alt="Copyright symbol" src="/img/copyright-symbol-svgrepo-com-1.svg" />
+              <p className="text-wrapper-46">2023 Restaurants. All Right Reserved. Designed by Mostafa Mahmoud Salah</p>
+            </div>
+            <div className="terms-of-service-wrapper">
+              <div className="terms-of-service">
+                Terms of Service&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Privacy Policy
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
